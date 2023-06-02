1d75d26dedf00385c4ce15e1b25c2ece7d6de8ab
Krxk <1246924370@qq.com>
2023-06-03 03:15:30 +0800

    backend

--- src/main/java/com/example/crackmathmodelplayer/CrackMathModelPlayerApplication.java
+++ src/main/java/com/example/crackmathmodelplayer/CrackMathModelPlayerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrackMathModelPlayerApplication {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to use crack_tool.\nDeveloped by Krxk");
+        System.out.println("Welcome to use crack_tool.\nDeveloped by Krxk.");
         SpringApplication.run(CrackMathModelPlayerApplication.class, args);
     }
 
