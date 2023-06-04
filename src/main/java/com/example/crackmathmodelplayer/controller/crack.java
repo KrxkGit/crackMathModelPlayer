79177bb90238478a662de8a816411938f88cbb24
Krxk <1246924370@qq.com>
2023-06-04 16:11:04 +0800

    backend

--- src/main/java/com/example/crackmathmodelplayer/controller/crack.java
+++ src/main/java/com/example/crackmathmodelplayer/controller/crack.java
@@ -3,13 +3,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.io.UnsupportedEncodingException;
 
 
 @RestController
 public class crack {
     @RequestMapping(value = "/2019/u3492432002/rzbfy.asp", method = RequestMethod.GET)
-    public String crack_api(String pwd, String code, @RequestParam(value = "uid", required = false) String uid, String pid, String v, String v2, String v3) throws UnsupportedEncodingException {
+    public String crack_api(String pwd, String code, @RequestParam(value = "uid", required = false) String uid, String pid, String v, String v2, String v3) {
         String api_key = "AAAAAA474B052F13794348074E005A76B91618771B00CA7309664D|0|||7ab6985c15c307f05303e8596765b79c";
         System.out.println("pwd:" + pwd);
         System.out.println("code: "+ code);
